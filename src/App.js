3488f7c3da3b24c61359c8bb584c9f377955ad17
Michael Kohler <me@michaelkohler.info>
2018-07-13 23:19:29 +0200

    Add 'Activities' Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Home from './components/Home/Home.js';
+import ActivitiesPage from './components/Activities/ActivitiesPage.js';
 import FAQ from './components/FAQ/FAQ.js';
 import EventGuide from './components/EventGuide/EventGuide.js';
 
@@ -19,6 +20,7 @@ class App extends Component {
         <BrowserRouter>
           <Switch>
             <Route exact path='/' component={Home}/>
+            <Route path='/activities' component={ActivitiesPage}/>
             <Route path='/faq' component={FAQ}/>
             <Route path='/eventguide' component={EventGuide}/>
           </Switch>
