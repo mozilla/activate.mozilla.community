e5a019c8ee1e1980fabea9df9949a53c2722e511
Michael Kohler <me@michaelkohler.info>
2018-09-27 19:46:25 +0200

    Update build tooling to same version as CRA2 has - includes node_modules babeling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import 'babel-polyfill';
 import 'fluent-intl-polyfill/compat';
 
 import 'bootstrap/dist/css/bootstrap.css';
