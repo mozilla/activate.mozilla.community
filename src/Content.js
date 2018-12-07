6cc63f2f71e04e2ac94f5c961eeec80edd0ac1e0
dwb17 <david@pointblank.gr>
2018-12-07 17:14:21 +0200

    window scrolltop on content change, smooth

--- src/Content.js
+++ src/Content.js
@@ -16,7 +16,11 @@ class Content extends Component {
   }
 
   componentDidUpdate() {
-    window.scrollTo(0,0);
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 
   render() {
