965083234fe2c886cd57542dc1e859e79dcb03b1
dwb17 <david@pointblank.gr>
2019-01-04 11:58:23 +0200

    code cleanup

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -6,12 +6,12 @@ import './NotFound.css';
 class NotFound extends Component {
   render() {
     return (
-      <section className="not-found">
+      <section className="not-found content-contained">
         <Localized id="not-found-title">
-          <h1>404 - Page not found!</h1>
+          <h1 className="title text--centered">404 - Page not found!</h1>
         </Localized>
         <Localized id="not-found-description">
-          <p className="text-center full-width">The page you are looking for could not be found. It may have been removed in the meantime or the URL might be wrong.</p>
+          <p className="text text--centered">The page you are looking for could not be found. It may have been removed in the meantime or the URL might be wrong.</p>
         </Localized>
       </section>
     );
